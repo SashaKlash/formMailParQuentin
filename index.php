d551463301f2cab58da34f747a6844033492d678
SashaKlash <sashaderuyver@gmail.com>
2021-11-16 17:20:48 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="">
+        <label for="name">Nom</label>
+        <input type="text" id="name" placeholder="Votre nom">
+        <label for="mail">Mail</label>
+        <input id="mail">
+        <label for="msg">Message</label>
+        <textarea id="msg" name="" placeholder="Votre message" required></textarea>
+    </form>
+</body>
+</html>
