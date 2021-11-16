3061c3ee8c9f0c8f64a29b130f46feae0f78eb09
SashaKlash <84438652+SashaKlash@users.noreply.github.com>
2021-11-16 17:26:03 +0100

    Update index.php
    mise à jour du code index.php

--- index.php
+++ index.php
@@ -1,19 +1,42 @@
+<?php
+require "./config.php";
+
+if (isset($_POST["name"]) && isset($_POST["mail"]) && isset($_POST["msg"])) {
+    $name = strip_tags(trim($_POST["name"]));
+    $mail = filter_var(strip_tags(trim($_POST["mail"])), FILTER_VALIDATE_EMAIL);
+    $message = strip_tags(trim($_POST["msg"]));
+
+    if ($name && $mail && $message) {
+        
+    } else {
+        echo "Error!";
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
+
     <form method="post" action="">
-        <label for="name">Nom</label>
-        <input type="text" id="name" placeholder="Votre nom">
-        <label for="mail">Mail</label>
-        <input id="mail">
-        <label for="msg">Message</label>
-        <textarea id="msg" name="" placeholder="Votre message" required></textarea>
+        <label for="name">Nom</label></br>
+        <input type="text" id="name" placeholder="Votre nom" name="name" required /></br>
+        <label for="mail">Mail</label></br>
+        <input type="text" id="mail" placeholder="Votre mail" name="mail" required /></br>
+        <label for="msg">Message</label></br>
+        <textarea id="msg" placeholder="Votre message" name="msg" required></textarea></br>
+        <input type="submit" value="send" />
     </form>
+
 </body>
+
 </html>
